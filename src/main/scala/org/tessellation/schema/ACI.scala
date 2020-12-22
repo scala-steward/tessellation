5add9600a5b9c3b0b09f81761845a0aec73c0132
Marcin Wadoń <mwadon@evojam.com>
2020-12-23 00:16:42 +0100

    WIP3

--- src/main/scala/org/tessellation/schema/ACI.scala
+++ src/main/scala/org/tessellation/schema/ACI.scala
@@ -1,6 +1,11 @@
 package org.tessellation.schema
 
+import cats.free.Free
+import cats.free.Free.liftF
+import cats.~>
 import higherkindness.droste.{Algebra, Coalgebra}
+import cats.Id
+import org.tessellation.schema.L1Consensus.Peer
 
 trait AciF[A]
 
@@ -28,4 +33,48 @@ object AciF {
     n => if (n <= 0) Return(n) else Execute(ApiCall(Request(n)))
   }
 
+}
+
+
+sealed trait L1ConsensusA[A]
+case class SelectFacilitators(peers: Set[Peer]) extends L1ConsensusA[Set[Peer]]
+case class CollectResponses[T](facilitators: Set[Peer]) extends L1ConsensusA[List[Response[T]]]
+case class ValidateResponses[T](responses: List[Response[T]]) extends L1ConsensusA[Option[T]]
+
+object L1Consensus {
+  type Peer = String
+  type Facilitators = Set[Peer]
+
+
+  type L1Consensus[A] = Free[L1ConsensusA, A]
+
+  def selectFacilitators(peers: Set[Peer]): L1Consensus[Set[Peer]] =
+    liftF[L1ConsensusA, Set[Peer]](SelectFacilitators(peers))
+
+  def collectResponses[T](facilitators: Set[Peer]): L1Consensus[List[Response[T]]] =
+    liftF[L1ConsensusA, List[Response[T]]](CollectResponses(facilitators: Set[Peer]))
+
+  def validateResponses[T](responses: List[Response[T]]): L1Consensus[Option[T]] =
+    liftF[L1ConsensusA, Option[T]](ValidateResponses(responses))
+
+  def consensus(peers: Set[Peer]): L1Consensus[Option[Int]] =
+    for {
+      facilitators <- selectFacilitators(peers)
+      responses <- collectResponses[Int](facilitators)
+      output <- validateResponses[Int](responses)
+    } yield output
+
+  def compiler: L1ConsensusA ~> Id = new (L1ConsensusA ~> Id) {
+    override def apply[A](fa: L1ConsensusA[A]): Id[A] = fa match {
+      case SelectFacilitators(peers) => {
+        peers.take(2).asInstanceOf[A]
+      }
+      case CollectResponses(facilitators) => {
+        facilitators.toList.map(peer => 2).asInstanceOf[A] // mocked api call
+      }
+      case ValidateResponses(responses) => {
+        (if (responses.toSet.size == 1) Some(responses.head) else None).asInstanceOf[A]
+      }
+    }
+  }
 }
